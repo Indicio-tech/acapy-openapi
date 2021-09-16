7e649ebe8f4e5f1231c3bcfdc8e3a27220485233
Daniel Bluhm <dbluhm@pm.me>
2021-09-16 12:52:54 -0400

    fix: adjust path in process script
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- scripts/process-openapi.sh
+++ scripts/process-openapi.sh
@@ -7,6 +7,6 @@ CONTAINER_NAME=process-openapi
 ${CONTAINER_RUNTIME} build -t ${CONTAINER_NAME} -f ../docker/Dockerfile.process-openapi .
 ${CONTAINER_RUNTIME} run --name ${CONTAINER_NAME} --rm -it \
     -v ../openapi.yml:/app/openapi.yml:z \
-    -v ./operation-id-map.yml:/app/operation-id-map.yml:z \
+    -v ../data/operation-id-map.yml:/app/operation-id-map.yml:z \
     -v ./process-openapi.py:/app/process-openapi.py:z \
     ${CONTAINER_NAME} python /app/process-openapi.py
