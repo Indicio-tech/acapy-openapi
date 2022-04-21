7ca32dff4122b5214c24a4f243aa76eac0e634a5
Colton Wolkins (Indicio work address) <colton@indicio.tech>
2022-04-21 16:44:27 -0600

    fix: Use absolute paths in process-openapi.sh
    Docker does not support local paths for volume mounts and requires that
    mounts use absolute paths. This commit switches to using absolute paths
    
    Signed-off-by: Colton Wolkins (Indicio work address) <colton@indicio.tech>

--- scripts/process-openapi.sh
+++ scripts/process-openapi.sh
@@ -6,7 +6,7 @@ CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-docker}
 CONTAINER_NAME=process-openapi
 ${CONTAINER_RUNTIME} build -t ${CONTAINER_NAME} -f ../docker/Dockerfile.process-openapi .
 ${CONTAINER_RUNTIME} run --name ${CONTAINER_NAME} --rm -it \
-    -v ../openapi.yml:/app/openapi.yml:z \
-    -v ../data/operation-id-map.yml:/app/operation-id-map.yml:z \
-    -v ./process-openapi.py:/app/process-openapi.py:z \
+	-v $(pwd)/../openapi.yml:/app/openapi.yml:z \
+	-v $(pwd)/../data/operation-id-map.yml:/app/operation-id-map.yml:z \
+	-v $(pwd)/../scripts/process-openapi.py:/app/process-openapi.py:z \
     ${CONTAINER_NAME} python /app/process-openapi.py
